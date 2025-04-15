6c58b7f5c1276651e73be5f24eaeb7612869ec02
harshit69x <mohantyharshit@gmail.com>
2025-04-16 00:14:50 +0530

    1st

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world application
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public String getGreeting() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testApp() {
+        App app = new App();
+        assertEquals("Hello World!", app.getGreeting());
+    }
+}
