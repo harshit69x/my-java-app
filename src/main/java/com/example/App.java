7695b3e341e2d67feea8922f0ebe04bb49572af6
harshit69x <mohantyharshit303@gmail.com>
2025-06-11 23:18:46 +0530

    ghf

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,8 +6,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-
     }
     
     public String getGreeting() {
